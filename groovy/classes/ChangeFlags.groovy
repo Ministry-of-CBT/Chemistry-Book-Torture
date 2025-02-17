6579473d53368c7bd4ea58c3924b5ca57c5f3128
planetme <planetmeksp@gmail.com>
2025-02-17 13:09:24 -0500

    change carnotite processing to yield sedimentary dust

--- groovy/classes/ChangeFlags.groovy
+++ groovy/classes/ChangeFlags.groovy
@@ -700,7 +700,7 @@ class ChangeFlags {
         oreProp.setOreByProducts(LimestoneTailings, LimestoneTailings, LimestoneTailings, LimestoneTailings);
 
         oreProp = Carnotite.getProperty(PropertyKey.ORE);
-        oreProp.setOreByProducts(GraniteTailings, GraniteTailings, GraniteTailings, GraniteTailings);
+        oreProp.setOreByProducts(LimestoneTailings, LimestoneTailings, LimestoneTailings, LimestoneTailings);
 
         oreProp = Cerussite.getProperty(PropertyKey.ORE);
         oreProp.setDirectSmeltResult(Lead);
